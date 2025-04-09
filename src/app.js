d282415f32c7875c8e1e7bbf17a9b76fc62a25f2
Juandomz91 <juandomz87@gmail.com>
2025-04-09 18:54:38 +0000

    vaya puto lio

--- src/app.js
+++ src/app.js
@@ -7,42 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   const numbers = ["A","2","4","6","8","10","J","K","Q"];
-  const symbols = ["♦", "♥", "♠", "♣"];
+  const simbols = ["Diamonds", "Spades", "Hearts", "Clubs"];
 
 
 
 
 //generamos un numero aleatorio para el array numérico rojo
-  const number = [Math.floor(Math.random()* 9];
-
-  
-//generamos un aleatorio para los simbolos
-  const symbol = [Math.floor(Math.random()* 4];
-  
+  const number = Math.floor(Math.random()* numbers.length );
 //guardamos el numero aleatorio en una constante
-  const randomNumberRoullette = getRandomRoullette;
+  const randomNumberRoullette = numbers[number];
+
+
+//generamos un aleatorio para los simbolos
+  const symbol = Math.floor(Math.random()* simbols.length );
+//guardamos el simbolo aleatorio en una constante
+  const randomSymbol = simbols[symbol];
+  
 
 //aplicamos los numeros y simbolos random a la targeta
-document.getElementById("num-top").innerText = getRandomsymbol;
-document.getElementById("num-card").innerText = getRandomRoullette;
-document.getElementById("num-bottom").innerText = getRandomsymbol;
+document.getElementById("num-top").innerText = randomSymbol;
+document.getElementById("num-card").innerText = randomNumberRoullette;
+document.getElementById("num-bottom").innerText = randomSymbol;
 
  
-//si el numero generado esta dentro del array...devuelve rojo, el resto será negro
-  function getColorByNumber(randomNumberRoullette){
-    if(redNumbers.includes(randomNumberRoullette)){
-      return 'red'
-    }
-    return 'black'
+//si el numero generado esta dentro del array...lo devuelve rojo, el resto será negro
+function getSuiteClass(simbols) {
+  switch (simbols) {
+    case "Diamonds": return "suit-diamonds";
+    case "Spades": return "suit-spades";
+    case "Hearts": return "suit-hearts";
+    case "Clubs": return "suit-clubs";
   }
-
-  //guardo la condición dentro de una constante
-  const ColorByNumber = getColorByNumber(randomNumberRoullette);
-
-
-  document.getElementById('roullette-number').innerText = randomNumberRoullette;
-  document.getElementById('roullette-number').classList.add(ColorByNumber);
-
+}
 
 
 };
